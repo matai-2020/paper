2796aa01094641d7b55269a10177a203da7f5f59
petemcnie <petemcnie0@gmail.com>
2020-06-05 07:17:14 +1200

    Added POST test, route name required to activate test

--- routes.test.js
+++ routes.test.js
@@ -1,16 +1,32 @@
 const request = require('supertest')
 const routes = require('./routes')
 
-test('all is good', () => {
+xtest('all is good', () => {
     expect(true).toBeTruthy()
 })
 
-test('get / is working correctly', done => {
+xtest('get / is working correctly', done => {
     request(routes)
         .get('/')
         .expect(200)
         .end((err, res) => {
             expect(res.text).toMatch('working correctly')
+            done()
+        })
+})
+
+xtest('POST /(nameofpostroutehere) responds with name of instructor', done => {
+    request(routes)
+        .post('nameofpostroutehere')
+        .send({
+            name: 'newTeacher',
+            id: 5
+        })
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .expect(200)
+        .end((err, res) => {
+            expect(res.text).toMatch('newTeacher')
+            expect(res.text).toMatch(5)
             done()
         })
 })
