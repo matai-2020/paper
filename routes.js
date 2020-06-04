d77eeeef9290a799f25af1476338b9ac03ae5bd4
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-05 10:29:44 +1200

    set up root route & server

--- routes.js
+++ routes.js
@@ -1,1 +1,19 @@
+const fs = require ('fs')
+const express = require('express')
+const router = express.Router()
+
+//root route
+router.get('/', (req, res) => {
+  res.redirect('/layouts/main')
+})
+
+
+
+
+//profile route
+
+
+
+
+//rating route
 
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express")
 
+const hbs = ("require express-handlebars")
+
+const router = require('./routes')
+
+const server = express()
+
+//Middleware
+
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({extended:false}))
+
+module.exports = server
