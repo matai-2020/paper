9e6979556bb871ba4112f067fb093b4fb7130b21
Ariel <33848430+arielwc@users.noreply.github.com>
2020-06-05 10:35:51 +1200

    created get profile route

--- routes.js
+++ routes.js
@@ -11,6 +11,16 @@ router.get('/', (req, res) => {
 
 
 //profile route
+router.get('/profile', (req, res) => {
+  fs.readFile('./teachers.json', 'utf8' (err, data) => {
+    if (err){
+      return res.status(500).send ('An error occured')
+    }
+    const teachers = JSON. parse(data)
+    res.render('index', teachers)
+    
+  }) 
+ })
 
 
 
