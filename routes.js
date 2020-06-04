87842a4ecd3c7f9710d81f28b6b6412b80ed83af
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-05 10:45:11 +1200

    profle/rating route

--- routes.js
+++ routes.js
@@ -4,26 +4,32 @@ const router = express.Router()
 
 //root route
 router.get('/', (req, res) => {
-  res.redirect('/layouts/main')
-})
-
-
-
-
-//profile route
-router.get('/profile', (req, res) => {
   fs.readFile('./teachers.json', 'utf8' (err, data) => {
     if (err){
       return res.status(500).send ('An error occured')
     }
     const teachers = JSON. parse(data)
-    res.render('index', teachers)
-    
+    res.render('/layouts/main')
+})
+
+//profile route
+router.get('/profile/:id', (req, res) => {
+  fs.readFile('./teachers.json', 'utf8' (err, data) => {
+    if (err){
+      return res.status(500).send ('An error occured')
+    }
+    const teachers = JSON. parse(data)
+    res.render('profile', teachers)
+
   }) 
  })
 
-
-
-
 //rating route
-
+router.get('/profile/rating/:id', (req, res) => {
+  fs.readFile('./teachers.json', 'utf8' (err, data) => {
+    if (err){
+      return res.status(500).send ('An error occured')
+    }
+    const teachers = JSON. parse(data)
+    res.render('rating', teachers)
+})
